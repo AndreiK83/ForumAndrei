fe21645a7633209c13af63ce37947c31836a4f8e
AndreiK83 <andreichiriac435@yahoo.com>
2018-09-16 11:37:56 +0300

    Commit
    Signed-off-by: AndreiK83 <andreichiriac435@yahoo.com>

--- src/main/java/ForumCapable.java
+++ src/main/java/ForumCapable.java
@@ -3,6 +3,7 @@ import java.util.List;
 public interface ForumCapable {
     public boolean login(String username, String password);
     public void logout();
+
     public void exitCategory();
     public void register(String username, String password, String mail);
     public void insertCategory(String subject);
